9a5edaffe16eb88b72a3830148436706dd00baa7
Maximus Aurelius Wiranata <83909501+maxx2002@users.noreply.github.com>
2023-07-31 13:48:15 +0700

    Initial Commit

---
+++ UbersnapChallenge/UbersnapChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  UbersnapChallengeApp.swift
+//  UbersnapChallenge
+//
+//  Created by Maximus Aurelius Wiranata on 31/07/23.
+//
+
+import SwiftUI
+
+@main
+struct UbersnapChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
